4dfc7880e10466bde222dd2002af2ba74e9ed081
Lucah <lucah.p.patel@gmail.com>
2025-11-13 13:23:28 +0100

    intervalle d'espace

--- plot.py
+++ plot.py
@@ -1,10 +1,31 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-output = pd.read_csv('output.csv')
-rho = output['rho'].values.to_numpy()
-U = output['U'].values.to_numpy()
-p = output['P'].values.to_numpy()
-np.array(output)
+df = pd.read_csv("output.csv")
+x = df['x'].to_numpy()
+rho = df['rho'].to_numpy()
+U = df['U'].to_numpy()
+p = df['p'].to_numpy()
+
+plt.figure(figsize=(15, 4))
+
+plt.subplot(1, 3, 1)
+plt.plot(x, rho, label='Density (rho)', color='blue')
+plt.title('Masse Volumique (t=0.2s)')
+plt.xlabel('Position (x)')
+plt.ylabel('Masse Volumique (rho)')
+
+plt.subplot(1, 3, 2)
+plt.plot(x, U, label='Velocity (U)', color='orange')
+plt.title('Vitesse (t=0.2s)')
+plt.xlabel('Position (x)')
+plt.ylabel('Vitesse (U)')
+
+plt.subplot(1, 3, 3)
+plt.plot(x, p, label='Pressure (P)', color='green')
+plt.title('Pression (t=0.2s)')
+plt.xlabel('Position (x)')
+plt.ylabel('Pression (P)')
+
+plt.show()
 
--- tchoc_exact.cpp
+++ tchoc_exact.cpp
@@ -4,46 +4,105 @@
 #include <fstream>
 using namespace std;
 
-double gamma;
+// Paramètres de Sod
+    double gamma=1.4;
 
-struct Zone {
-    double p;
-    double rho;
-    double T;
-    double U;
-};
+    // Zone Gauche
+    double p_L = 8.0;
+    double rho_L = 10.0/gamma;
+    double a_L = sqrt(gamma * p_L / rho_L);
+    double U_L = 0.0;
 
+// Zone Droite
+double p_R = 1.0;
+double rho_R = 1.0/gamma;
+double a_R = sqrt(gamma * p_R / rho_R);
+double U_R = 0.0;
+
+double fms(double Ms) {
+    return Ms;
+}
 
 vector<vector<double>> tchoc_exact(vector<double> x, double x0, double t) {
     vector<vector<double>> result;
+    double Ms = fms(0.0);
+
+    // Zone 1 (après onde de choc)
+    double rho_1 = rho_R * ((gamma + 1) * Ms * Ms) / ((gamma - 1) * Ms * Ms + 2);
+    double U_1 = 2/(gamma+1)*(Ms - 1/Ms);
+    double p_1 = p_R * (2*gamma/(gamma+1)*Ms*Ms - (gamma-1)/(gamma+1));
+    double a_1 = sqrt(gamma * p_1 / rho_1);
+    
+    // Zone 2 (entre onde de choc et contact)
+    double U_2 = U_1;
+    double p_2 = p_1;
+    double rho_2 = rho_L * pow((p_2/p_L), (1.0/gamma));
+    double a_2 = a_L - (gamma - 1)/2 * U_2;
+
+    // Positions des discontinuités
+    double x_1 = x0 - a_L * t; // Front de l'onde rarefaction
+    double x_2 = x0 + (U_2 - a_2) * t; // Fin de l'onde rarefaction
+    double x_3 = x0 + U_2 * t; // Contact
+    double x_4 = x0 + Ms * t; // Onde de choc
+
+    // Remplissage des résultats
+    vector<double> rho(x.size()), U(x.size()), p(x.size()), a(x.size());
+    for(size_t i = 0; i < x.size(); i++) {
+        if(x[i] < x_1) {
+            U[i] = U_L;
+            a[i] = a_L;
+            p[i] = p_L;
+            rho[i] = rho_L;
+        } else if(x[i] < x_2) {
+            U[i] = 2/(gamma+1)*(a_L + (x[i]-x0)/t);
+            a[i] = 2/(gamma+1)*(a_L - (gamma - 1)/2 * (x[i]-x0)/t);
+            p[i] = p_L * pow((a[i]/a_L), (2.0*gamma/(gamma-1)));
+            rho[i] = gamma * p[i] / (a[i] * a[i]);
+        } else if(x[i] < x_3) {
+            U[i] = U_2;
+            a[i] = a_2;
+            p[i] = p_2;
+            rho[i] = rho_2;
+        } else if(x[i] < x_4) {
+            U[i] = U_1;
+            a[i] = a_1;
+            p[i] = p_1;
+            rho[i] = rho_1;
+        } else {
+            U[i] = U_R;
+            a[i] = a_R;
+            p[i] = p_R;
+            rho[i] = rho_R;
+        }
+    }
+    result.push_back(rho);
+    result.push_back(U);
+    result.push_back(p);
     return result;
 }
 
+
 int main() {
+
+    // Paramètres ex 10.1
     int n = 81;
+    double x0 = 0.5;
+    double t = 0.2;
+
     vector<double> x(n);
     for(int i=0; i<n; i++) {
         x[i] = i * (1.0 / (n - 1));
     }
 
-    double x0 = 0.5;
-    double t = 0.2;
-    gamma = 1.4;
-    Zone zone_L;
-    zone_L.p = 8.0;
-    zone_L.rho = 10.0/gamma;
     vector<vector<double>> result = tchoc_exact(x, x0, t);
 
 
     ofstream file("output.csv");
     file << "x,rho,U,p\n";
-    for (int j = 0; j < x.size(); ++j) {
-        file << x[j];
-        for (int i = 0; i < result.size(); ++i) {
-            file << ',' << result[i][j];
-        }
-    file << '\n';
+    for(int i = 0; i < n; ++i) {
+        file << x[i] << ',' << result[0][i] << ',' << result[1][i] << ',' << result[2][i] << '\n';
     }
+    
     file.close();
     return 0;
 }
