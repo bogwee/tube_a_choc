24fe4fd8b60d0bbb4306d6a0513d32749eb37519
Lucah <lucah.p.patel@gmail.com>
2026-02-18 16:41:37 +0100

    roe C++

--- tchoc_num.cpp
+++ tchoc_num.cpp
@@ -55,6 +55,65 @@ vector<double> flux_centre(vector<double> w) {
     return f;
 }
 
+vector<double> dissipation_roe(vector<double> wl, vector<double> wr) {
+    double rho_l = wl[0];
+    double rho_U_l = wl[1];
+    double E_l = wl[2];
+    double U_l = rho_U_l / rho_l;
+    double p_l = (g - 1) * (E_l - rho_U_l * U_l / 2.0);
+    double H_l = (E_l + p_l) / rho_l;
+    double a_l = sqrt(g * p_l / rho_l);
+
+    double rho_r = wr[0];
+    double rho_U_r = wr[1];
+    double E_r = wr[2];
+    double U_r = rho_U_r / rho_r;
+    double p_r = (g - 1) * (E_r - rho_U_r * U_r / 2.0);
+    double H_r = (E_r + p_r) / rho_r;
+    double a_r = sqrt(g * p_r / rho_r);
+
+    double d_rho = rho_r - rho_l;
+    double d_rho_U = rho_U_r - rho_U_l;
+    double d_E = E_r - E_l;
+
+    double R = sqrt(rho_r / rho_l);
+    double rho_bar = R * rho_l;
+    double U_bar = (R * U_r + U_l) / (R + 1);
+    double H_bar = (R * H_r + H_l) / (R + 1);
+    double a_bar = sqrt((g - 1) * (H_bar - U_bar * U_bar / 2.0));
+
+    vector<double> l = {fabs(U_bar - a_bar), fabs(U_bar), fabs(U_bar + a_bar)};
+
+    vector<vector<double>> v = {{1, U_bar - a_bar, H_bar - U_bar * a_bar}, 
+                                {1, U_bar, 0.5 * U_bar * U_bar}, 
+                                {1, U_bar + a_bar, H_bar + U_bar * a_bar}};
+
+    double alpha1 = 0.5 * (g - 1) * (U_bar * U_bar)/(a_bar * a_bar);
+    double alpha2 = (g - 1)/(a_bar * a_bar);
+
+    vector<double> alpha = {0.5 * (alpha1 + U_bar/a_bar) * d_rho - 0.5 * (alpha2 * U_bar + 1/a_bar) * d_rho_U + 0.5 * alpha2 * d_E, 
+                            (1 - alpha1) * d_rho + alpha2 * U_bar * d_rho_U - alpha2 * d_E, 
+                            0.5 * (alpha1 - U_bar/a_bar) * d_rho - 0.5 * (alpha2 * U_bar - 1/a_bar) * d_rho_U + 0.5 * alpha2 * d_E};
+
+    vector<double> dissip(3);
+    for(int i=0; i<3; i++) {
+        for (int j = 0; j < 3; j++){
+            dissip[i] += l[j] * alpha[j] * v[j][i];
+        }
+        
+    }
+
+    return dissip;
+}
+
+vector<double> flux_roe(vector<double> w_L, vector<double> w_R) {
+    vector<double> phi(3);
+    for(int i=0; i<3; i++) {
+        phi[i] = 0.5 * (flux_centre(w_L)[i] + flux_centre(w_R)[i] - dissipation_roe(w_L, w_R)[i]);
+    }
+    return phi;
+}
+
 vector<vector<double>> lax_wendroff(vector<vector<double>> W, double T, double dx, double cfl=0.95) {
     int N = W[0].size();
     double t = 0.0;
@@ -76,6 +135,35 @@ vector<vector<double>> lax_wendroff(vector<vector<double>> W, double T, double d
             auto f_tilde_left  = flux_centre({W_tilde[0][i-1], W_tilde[1][i-1], W_tilde[2][i-1]});
             for(int j=0; j<3; j++) {
                 W_new[j][i] = W[j][i] - dt/dx * (f_tilde_right[j] - f_tilde_left[j]);
+            }
+        }
+        W = W_new;
+        t += dt;
+    }
+    return W;
+}
+
+vector<vector<double>> lax_wendroff_artificiel(vector<vector<double>> W, double T, double dx, double D, double cfl=0.95) {
+    int N = W[0].size();
+    double t = 0.0;
+    vector<vector<double>> W_tilde(3, vector<double>(N-1));
+    vector<vector<double>> W_new = W;
+    double dt = calc_dt(W, dx, cfl);
+    while(t < T) {
+        double dt = calc_dt(W, dx, cfl);
+        if (t + dt > T) dt = T - t;
+        for(int i=0; i<N-1; i++) {
+            auto f_right = flux_centre({W[0][i+1], W[1][i+1], W[2][i+1]});
+            auto f_left  = flux_centre({W[0][i], W[1][i], W[2][i]});
+            for(int j=0; j<3; j++) {
+                W_tilde[j][i] = (W[j][i]+W[j][i+1])/2.0 - dt/(2*dx) * (f_right[j] - f_left[j] + D * dx * (2*W[j][i] - W[j][i+1] - W[j][i-1]));
+            }
+        }
+        for(int i=1; i<N-1; i++) {
+            auto f_tilde_right = flux_centre({W_tilde[0][i], W_tilde[1][i], W_tilde[2][i]});
+            auto f_tilde_left  = flux_centre({W_tilde[0][i-1], W_tilde[1][i-1], W_tilde[2][i-1]});
+            for(int j=0; j<3; j++) {
+                W_new[j][i] = W[j][i] - dt/dx * (f_tilde_right[j] - f_tilde_left[j]+ D * dx * (2*W_tilde[j][i] - W_tilde[j][i+1] - W_tilde[j][i-1]));
             }
         }
         W = W_new;
@@ -114,12 +202,65 @@ vector<vector<double>> maccormack(vector<vector<double>> W, double T, double dx,
     return W;
 }
 
+vector<vector<double>> maccormack_artificiel(vector<vector<double>> W, double T, double dx, double D, double cfl=0.95) {
+    int N = W[0].size();
+    double t = 0.0;
+    vector<vector<double>> W_predict(3, vector<double>(N-1));
+    vector<vector<double>> W_correct = W;
+    while(t < T) {
+        double dt = calc_dt(W, dx, cfl);
+        if (t + dt > T) dt = T - t;
+
+        for(int i=0; i<N-1; i++) {
+            auto f_right = flux_centre({W[0][i+1], W[1][i+1], W[2][i+1]});
+            auto f_left  = flux_centre({W[0][i], W[1][i], W[2][i]});
+            for(int j=0; j<3; j++) {
+                W_predict[j][i] = W[j][i] - dt/dx * (f_right[j] - f_left[j] + D * dx * (2*W[j][i] - W[j][i+1] - W[j][i-1]));
+            }
+        }
+
+        for(int i=1; i<N-1; i++) {
+            auto f_predict_right = flux_centre({W_predict[0][i], W_predict[1][i], W_predict[2][i]});
+            auto f_predict_left  = flux_centre({W_predict[0][i-1], W_predict[1][i-1], W_predict[2][i-1]});
+            for(int j=0; j<3; j++) {
+                W_correct[j][i] = 0.5 * (W[j][i] + W_predict[j][i]) - 0.5 * dt/dx * (f_predict_right[j] - f_predict_left[j] + D * dx * (2*W_predict[j][i] - W_predict[j][i+1] - W_predict[j][i-1]));
+            }
+        }
+        W = W_correct;
+        t += dt;
+    }
+    return W;
+}
+
+vector<vector<double>> roe(vector<vector<double>> W, double T, double dx, double cfl=0.95) {
+    int N = W[0].size();
+    double t = 0.0;
+    vector<vector<double>> W_new = W;
+    while(t < T) {
+        double dt = calc_dt(W, dx, cfl);
+        if (t + dt > T) dt = T - t;
+
+        for(int i=1; i<N-1; i++) {
+            auto phi_right = flux_roe({W[0][i], W[1][i], W[2][i]}, {W[0][i+1], W[1][i+1], W[2][i+1]});
+            auto phi_left  = flux_roe({W[0][i-1], W[1][i-1], W[2][i-1]}, {W[0][i], W[1][i], W[2][i]});
+            for(int j=0; j<3; j++) {
+                W_new[j][i] = W[j][i] - dt/dx * (phi_right[j] - phi_left[j]);
+            }
+        }
+        W = W_new;
+        t += dt;
+    }
+    return W;
+}
+
 int main() {
 
     // Paramètres ex 10.1
     int n = 101;
     double x0 = 0.5;
     double t = 0.2;
+    double D = 8.0;
+    double dx = 1.0/(n-1);
 
     vector<vector<double>> W(3, vector<double>(n));
     for(int i=0; i<n; i++) {
@@ -128,7 +269,7 @@ int main() {
         W[2][i] = (i < n/2) ? (p_L/(g-1) + 0.5 * W[1][i] * U_L) : (p_R/(g-1) + 0.5 * W[1][i] * U_R);
     }
 
-    vector<vector<double>> result = maccormack(W, t, 1.0/(n-1));
+    vector<vector<double>> result = roe(W, t, dx);
 
     for(int i=0; i<n; i++) {
         result[1][i] /= result[0][i]; // Convertir rho_U en U
@@ -139,7 +280,7 @@ int main() {
     ofstream file("output_num.csv");
     file << "x,rho,U,p\n";
     for(int i = 0; i < n; ++i) {
-        file << i * (1.0 / (n - 1)) << ',' << result[0][i] << ',' << result[1][i] << ',' << result[2][i] << '\n';
+        file << i * dx << ',' << result[0][i] << ',' << result[1][i] << ',' << result[2][i] << '\n';
     }
     
     file.close();
